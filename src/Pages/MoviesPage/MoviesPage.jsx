7838d5de99def00fa4b8b71529690eb8dfdfb636
Samuel Budy Dharwan <dharwan.helena@gmail.com>
2022-07-21 22:05:20 +0700

    update: change inside MoviesPage

--- src/Pages/MoviesPage/MoviesPage.jsx
+++ src/Pages/MoviesPage/MoviesPage.jsx
@@ -3,21 +3,25 @@ import Container from "../../Components/Container.jsx";
 import NowPlayingMovies from "./components/NowPlayingMovies.jsx";
 import PopularMovies from "./components/PopularMovies.jsx";
 import TopRatedMovies from "./components/TopRatedMovies.jsx";
-import setTitle from "../../utils/useTitle"
+import OnTheAgenda from "./components/OnTheAgenda.jsx";
+import UserCollections from "./components/UserCollections.jsx";
+import setTitle from "../../utils/useTitle";
 
 const MoviesPage = () => {
-    setTitle('Movie List')
-    return (
-        <>
-            <div className="bg-netflix-dark text-white">
-                <Container>
-                    <NowPlayingMovies />
-                    <PopularMovies />
-                    <TopRatedMovies />
-                </Container>
-            </div>
-        </>
-    )
-}
+  setTitle("Movie List");
+  return (
+    <>
+      <div className="bg-netflix-dark text-white">
+        <Container>
+          <NowPlayingMovies />
+          <PopularMovies />
+          <TopRatedMovies />
+          <OnTheAgenda />
+          <UserCollections />
+        </Container>
+      </div>
+    </>
+  );
+};
 
-export default MoviesPage
+export default MoviesPage;
